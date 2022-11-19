769445f786c92edba58bf0dec87c0018dff6c98a
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-11-19 22:15:36 +0530

    add javascript logic to use whatsapp api to send message to a number

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,30 +1,31 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import './Form.css';
+import { useState } from 'react';
 
 export default function Form() {
+    const [number, setNumber] = useState(0);
     return (
-        // <Box
-        //     sx={{
-        //         width: 500,
-        //         maxWidth: '100%',
-        //     }}
-        // >
-            <div className="formData">
-                <TextField
-                    style={{ marginBottom: 20 }}
-                    required
-                    id="outlined-number"
-                    label="Phone Number"
-                    type="tel"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                />
-                <Button variant="contained">Send Message</Button>
-            </div>
-        // </Box>
+        <div className="formData">
+            <TextField
+                style={{ marginBottom: 20 }}
+                required
+                id="outlined-number"
+                label="Phone Number"
+                type="tel"
+                placeholder="Enter your phone number"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                onChange={(e) => {
+                    setNumber(e.target.value);
+                }}
+            />
+            <a className='anchor' href={number ? `http://wa.me/+91${number}` : "/#"} target="_self" rel="noopener noreferrer">
+                <Button variant="contained" color="success">Send Message</Button>
+            </a>
+
+        </div>
     );
 }
